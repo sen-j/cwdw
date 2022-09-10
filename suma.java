4c6aaf8c64a994ec93b5baeea945c92c01342f2a
senj <kubasendek@protonmail.com>
2022-09-10 17:29:38 +0200

    added suma.java

---
+++ suma.java
@@ -0,0 +1,18 @@
+// Napisz program, który liczy sumê dziesiêciu liczb z ci¹gu 5,10,15,...
+
+
+public class zad3 {
+
+	public static void main(String[] args) {
+		
+		int suma=0;
+		System.out.println("Suma z liczb: ");
+		for(int i=1; i<=10; i++)
+		{
+			suma+=i*5;
+			System.out.print(i*5+" ");
+		}
+		System.out.println("\nto: "+suma);
+	}
+
+}
