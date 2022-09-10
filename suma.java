0826df653cab9cab12de3b34ff34c9a4e9e85b23
senj <kubasendek@protonmail.com>
2022-09-10 17:49:56 +0200

    added a message for user at the beginning

--- suma.java
+++ suma.java
@@ -3,6 +3,7 @@ public class zad3 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Liczenie sumy");
 		int suma=0;
 		System.out.println("Suma z liczb: ");
 		for(int i=1; i<=15; i++)
